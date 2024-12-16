53a20ba3f411f32fbdf3f515aa9adb821aba849c
Cristian Mauricio <cristian.mauricio@visma.com>
2024-12-16 18:02:39 -0500

    Agregando proyecto

---
+++ src/test/java/com/example/liquorstoreapi/LiquorStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liquorstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquorStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
